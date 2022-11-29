d6cbafe2884a3439a2c083b4470bfc4ef8cb3585
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2022-11-29 12:36:06 +0700

    Update Color_detection.py

--- Color_detection.py
+++ Color_detection.py
@@ -1,23 +1,35 @@
-import cv2 
+from __future__ import print_function
+from __future__ import division
+import cv2
 import numpy as np
+import argparse
+from math import atan2, cos, sin, sqrt, pi
 
 cap = cv2.VideoCapture(0)
 
 #trackbar callback fucntion to update HSV value
-def callback(x):
-	global H_low,H_high,S_low,S_high,V_low,V_high
-	#assign trackbar position value to H,S,V High and low variable
-	H_low = cv2.getTrackbarPos('low H','controls')
-	H_high = cv2.getTrackbarPos('high H','controls')
-	S_low = cv2.getTrackbarPos('low S','controls')
-	S_high = cv2.getTrackbarPos('high S','controls')
-	V_low = cv2.getTrackbarPos('low V','controls')
-	V_high = cv2.getTrackbarPos('high V','controls')
+def trackbar():
+    global H_low,H_high,S_low,S_high,V_low,V_high
+    H_low = cv2.getTrackbarPos('low H','controls')
+    H_high = cv2.getTrackbarPos('high H','controls')
+    S_low = cv2.getTrackbarPos('low S','controls')
+    S_high = cv2.getTrackbarPos('high S','controls')
+    V_low = cv2.getTrackbarPos('low V','controls')
+    V_high = cv2.getTrackbarPos('high V','controls')
+
+def empty(a):
+    pass
+
+def momen():
+    M = cv2.moments(c)                                                          #menghitung nilai momen pada contour
+    cx = int(M['m10']/M['m00'])                                                 #menghitung nilai koordinat x berdasarkan momen
+    cy = int(M['m01']/M['m00'])                                                 #menghitung nilai koordinat y berdasarkan momen
+    cv2.circle(frame, (cx, cy), 5, (0, 0, 255), 1)                              #menggambar lingkaran pada titik koordinat kontur dengan jari jari 5
 
 
 #create a seperate window named 'controls' for trackbar
 cv2.namedWindow('controls',2)
-cv2.resizeWindow("controls", 550,10);
+cv2.resizeWindow("controls", 250,10);
 
 
 #global variable
@@ -29,27 +41,47 @@ V_low= 0
 V_high = 255
 
 #create trackbars for high,low H,S,V 
-cv2.createTrackbar('low H','controls',0,179,callback)
-cv2.createTrackbar('high H','controls',179,179,callback)
+cv2.createTrackbar('low H','controls',0,179,empty)
+cv2.createTrackbar('high H','controls',179,179,empty)
 
-cv2.createTrackbar('low S','controls',0,255,callback)
-cv2.createTrackbar('high S','controls',255,255,callback)
+cv2.createTrackbar('low S','controls',0,255,empty)
+cv2.createTrackbar('high S','controls',255,255,empty)
 
-cv2.createTrackbar('low V','controls',0,255,callback)
-cv2.createTrackbar('high V','controls',255,255,callback)
+cv2.createTrackbar('low V','controls',0,255,empty)
+cv2.createTrackbar('high V','controls',255,255,empty)
 
 while(1):
 
-    _, frame = cap.read()                               #karena nilai ret tidak di akan di proses maka dapat di ganti dengan _
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)          #mengubah gambar dari bgr menjadi hsv, gambar yang di ambil oleh opencv selalu menggunakan urutan bgr bukan rgb
+    _, frame = cap.read()                   
 
-    lower_blue = np.array([H_low,S_low,V_low])                  #nilai-nilai hsv untuk threshold bawah
-    upper_blue = np.array([H_high,S_high,V_high])                #nilai-nilai hsv untuk threshold atas
+    scale_percent = 60 # percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
 
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)      #nilai-nilai pada variabel hsv yang masuk kedalam threshold lower_blue dan upper_blue akan diberi angka 1 atau high sedangkan yang tidak masuk diberi angka 0 atau low
-    res = cv2.bitwise_and(frame,frame, mask= mask)       #melakukan operasi 'and' antara gambar awal (frame) dengan hasil pemilihan (mask) sehingga didapatkan gambar dengan hanya area seleksi yang muncul
+    # resize image
+    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)             
 
-    cv2.imshow('frame',frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)         
+
+    trackbar()
+
+    lower_blue = np.array([H_low,S_low,V_low])           
+    upper_blue = np.array([H_high,S_high,V_high])        
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)      
+    res = cv2.bitwise_and(frame,frame, mask= mask)      
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)      
+    for i, c in enumerate(contours):                                                
+        area = cv2.contourArea(c)                                                   
+        if area < 1e2 or 1e5 < area:                                                
+            continue
+        cv2.drawContours(frame, contours, i, (0, 0, 255), 2)       
+
+        momen()                 
+
+    #cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     #cv2.imshow('res',res)
     #cv2.imshow('hsv',hsv)
@@ -60,9 +92,3 @@ while(1):
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-#why using hsv instead of bgr:
-#Because the R, G, and B components of an objectÃ¢â‚¬â„¢s color in a digital image are all correlated with the amount of light hitting the object,
-#and therefore with each other, image descriptions in terms of those components make object discrimination difficult. 
-#Descriptions in terms of hue/lightness/chroma or hue/lightness/saturation are often more relevant.
